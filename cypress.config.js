25c7bb71d9ffb8b2b4ade8a0e32f6d4599870b32
Caroline Rivera <caroline.fernandes.rivera@gmail.com>
2025-08-10 11:03:04 -0300

    fix config error

--- cypress.config.js
+++ cypress.config.js
@@ -6,8 +6,6 @@ module.exports = defineConfig({
     baseUrl: 'https://www.saucedemo.com',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.js',
-    video: process.env.CI ? false : true,
-    screenshotOnRunFailure: process.env.CI ? false : true,
     setupNodeEvents(on, config) {
       on('task', {
         log(message) {
@@ -22,7 +20,10 @@ module.exports = defineConfig({
     }
   },
   retries: {
-    runMode: 1,   // 1 retry on mode headless/CI
-    openMode: 0,  // no retry on open mode
-  }
+    runMode: 1,
+    openMode: 0,
+  },
+  
+  video: process.env.CI ? false : true,
+  screenshotOnRunFailure: process.env.CI ? false : true,
 });
